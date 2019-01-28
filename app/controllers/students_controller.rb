6b9d1a011dcfb006103dbe4042d1e2527448aba2
Merline Belizaire <merline@noukod.com>
2019-01-28 17:45:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,10 +13,11 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new
-  @student.first_name = params[:first_name]
-  @student.last_name = params[:last_name]
-  @student.save
+  #   @student = Student.new
+  # @student.first_name = params[:first_name]
+  # @student.last_name = params[:last_name]
+  # @student.save
+  Post.create(first_name: params[:first_name], last_name: params[:last_name])
   redirect_to post_path(@student)
   end
 
