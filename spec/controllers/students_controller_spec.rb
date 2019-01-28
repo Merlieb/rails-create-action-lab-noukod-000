617269b2d0eda25d820ad66619c1b4b373d35c6d
Merline Belizaire <merline@noukod.com>
2019-01-28 17:53:37 +0000

    Automatically backed up by Learn

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,7 +1,7 @@
 # require 'rails_helper'
-# 
+#
 # describe StudentsController do
-# 
+#
 #   describe "POST create" do
 #     context "with valid attributes" do
 #       it "creates a new student" do
@@ -9,12 +9,12 @@
 #           post :create, { :first_name => "Sam", :last_name => "Smith" }
 #         }.to change(Student,:count).by(1)
 #       end
-# 
+#
 #       it "redirects to the new student" do
 #         post :create, { :first_name => "Sam", :last_name => "Smith" }
 #         expect(response).to redirect_to Student.last
 #       end
 #     end
 #   end
-# 
+#
 # end
